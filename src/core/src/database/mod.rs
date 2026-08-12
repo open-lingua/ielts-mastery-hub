d9d258bf56958f49c05af74acce3c11057c2ed68
Luis Miguel Báez <es.luismiguelbaez@gmail.com>
2026-08-12 16:12:45 -0500

    feat: run seeds automatically after migrations on DB init

--- src/core/src/database/mod.rs
+++ src/core/src/database/mod.rs
@@ -22,5 +22,6 @@ pub async fn init(app_handle: &tauri::AppHandle) -> Result<Db, AppError> {
         .await?;
     sqlx::query("PRAGMA foreign_keys=ON").execute(&pool).await?;
     sqlx::migrate!("./src/database/migrations").run(&pool).await?;
+    sqlx::migrate!("./src/database/seeds").run(&pool).await?;
     Ok(pool)
 }
