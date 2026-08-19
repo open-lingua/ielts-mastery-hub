0da6be387c8528087d8e3173ad63cd658f77cf39
Luis Miguel Báez <es.luismiguelbaez@gmail.com>
2026-08-19 17:47:10 -0500

    Register tauri-plugin-dialog in app builder

--- src/core/src/lib.rs
+++ src/core/src/lib.rs
@@ -10,6 +10,7 @@ use tauri::Manager;
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_dialog::init())
         .setup(|app| {
             if std::env::var("OPEN_DEVTOOLS").as_deref() == Ok("true") {
                 if let Some(window) = app.get_webview_window("main") {
