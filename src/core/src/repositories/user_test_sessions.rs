64b866c6c46f8eaccef2e8ce7987fbdd4b375f01
Luis Miguel Báez <es.luismiguelbaez@gmail.com>
2026-08-19 20:06:40 -0500

    refactor: remove UserTestSession structs from repositories/user_test_sessions.rs

--- src/core/src/repositories/user_test_sessions.rs
+++ src/core/src/repositories/user_test_sessions.rs
@@ -1,43 +1,8 @@
-use serde::{Deserialize, Serialize};
-
 use crate::database::Db;
 use crate::error::AppError;
-
-#[derive(Debug, Serialize, Deserialize, Clone, sqlx::FromRow)]
-pub struct UserTestSession {
-    pub id: String,
-    pub user_id: String,
-    pub test_id: String,
-    pub test_type: String,
-    pub status: String,
-    pub progress_percent: i64,
-    pub score_band: Option<f64>,
-    pub attempt_number: i64,
-    pub answers: Option<String>,
-    pub feedback_data: Option<String>,
-    pub started_at: String,
-    pub completed_at: Option<String>,
-    pub last_active_at: String,
-    pub created_at: String,
-}
-
-#[derive(Debug, Deserialize)]
-pub struct CreateUserTestSession {
-    pub test_id: String,
-    pub test_type: String,
-    pub attempt_number: Option<i64>,
-}
-
-#[derive(Debug, Deserialize)]
-pub struct UpdateUserTestSession {
-    pub status: Option<String>,
-    pub progress_percent: Option<i64>,
-    pub score_band: Option<f64>,
-    pub answers: Option<String>,
-    pub feedback_data: Option<String>,
-    pub completed_at: Option<String>,
-    pub last_active_at: Option<String>,
-}
+use crate::models::user_test_sessions::{
+    CreateUserTestSession, UpdateUserTestSession, UserTestSession,
+};
 
 pub async fn find_by_id(
     pool: &Db,
