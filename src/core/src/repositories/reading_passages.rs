cd6341bf81fc7c258291400a297510eda39d7ef4
Luis Miguel Báez <es.luismiguelbaez@gmail.com>
2026-08-19 20:06:36 -0500

    refactor: remove ReadingPassage structs from repositories/reading_passages.rs

--- src/core/src/repositories/reading_passages.rs
+++ src/core/src/repositories/reading_passages.rs
@@ -1,35 +1,6 @@
-use serde::{Deserialize, Serialize};
-
 use crate::database::Db;
 use crate::error::AppError;
-
-#[derive(Debug, Serialize, Deserialize, Clone, sqlx::FromRow)]
-pub struct ReadingPassage {
-    pub id: String,
-    pub test_id: String,
-    pub passage_number: i64,
-    pub title: String,
-    pub content: String,
-    pub notes: Option<String>,
-    pub created_at: String,
-}
-
-#[derive(Debug, Deserialize)]
-pub struct CreateReadingPassage {
-    pub test_id: String,
-    pub passage_number: Option<i64>,
-    pub title: Option<String>,
-    pub content: Option<String>,
-    pub notes: Option<String>,
-}
-
-#[derive(Debug, Deserialize)]
-pub struct UpdateReadingPassage {
-    pub passage_number: Option<i64>,
-    pub title: Option<String>,
-    pub content: Option<String>,
-    pub notes: Option<String>,
-}
+use crate::models::reading_passages::{CreateReadingPassage, ReadingPassage, UpdateReadingPassage};
 
 pub async fn find_by_id(
     pool: &Db,
