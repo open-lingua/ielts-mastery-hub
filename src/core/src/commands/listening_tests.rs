8146c7bbe4db0452e27fcff380ade2b24d375385
Luis Miguel Báez <es.luismiguelbaez@gmail.com>
2026-08-19 20:06:23 -0500

    refactor: import ListeningTest structs from models in listening_tests.rs

--- src/core/src/commands/listening_tests.rs
+++ src/core/src/commands/listening_tests.rs
@@ -2,9 +2,7 @@ use tauri::State;
 
 use crate::database::Db;
 use crate::repositories::listening_tests as repo;
-use crate::repositories::listening_tests::{
-    CreateListeningTest, ListeningTest, UpdateListeningTest,
-};
+use crate::models::listening_tests::{CreateListeningTest, ListeningTest, UpdateListeningTest};
 
 #[tauri::command]
 pub async fn get_listening_tests(
