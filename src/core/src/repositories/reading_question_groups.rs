e295a835b282965fe2fc4fa6aac2bbd6065e5d5a
Luis Miguel Báez <es.luismiguelbaez@gmail.com>
2026-08-19 20:06:37 -0500

    refactor: remove ReadingQuestionGroup structs from repositories/reading_question_groups.rs

--- src/core/src/repositories/reading_question_groups.rs
+++ src/core/src/repositories/reading_question_groups.rs
@@ -1,50 +1,8 @@
-use serde::{Deserialize, Serialize};
-
 use crate::database::Db;
 use crate::error::AppError;
-
-#[derive(Debug, Serialize, Deserialize, Clone, sqlx::FromRow)]
-pub struct ReadingQuestionGroup {
-    pub id: String,
-    pub passage_id: String,
-    pub group_order: i64,
-    pub question_type: String,
-    pub instructions: String,
-    pub word_limit: Option<String>,
-    pub has_word_bank: bool,
-    pub word_bank: String,
-    pub sequential_order: bool,
-    pub multiple_selection: bool,
-    pub select_count: i64,
-    pub created_at: String,
-}
-
-#[derive(Debug, Deserialize)]
-pub struct CreateReadingQuestionGroup {
-    pub passage_id: String,
-    pub group_order: Option<i64>,
-    pub question_type: Option<String>,
-    pub instructions: Option<String>,
-    pub word_limit: Option<String>,
-    pub has_word_bank: Option<bool>,
-    pub word_bank: Option<String>,
-    pub sequential_order: Option<bool>,
-    pub multiple_selection: Option<bool>,
-    pub select_count: Option<i64>,
-}
-
-#[derive(Debug, Deserialize)]
-pub struct UpdateReadingQuestionGroup {
-    pub group_order: Option<i64>,
-    pub question_type: Option<String>,
-    pub instructions: Option<String>,
-    pub word_limit: Option<String>,
-    pub has_word_bank: Option<bool>,
-    pub word_bank: Option<String>,
-    pub sequential_order: Option<bool>,
-    pub multiple_selection: Option<bool>,
-    pub select_count: Option<i64>,
-}
+use crate::models::reading_question_groups::{
+    CreateReadingQuestionGroup, ReadingQuestionGroup, UpdateReadingQuestionGroup,
+};
 
 pub async fn find_by_id(
     pool: &Db,
