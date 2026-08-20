3ac050b661388f0acb94c8a547f6a045ac687d2b
Luis Miguel Báez <es.luismiguelbaez@gmail.com>
2026-08-19 20:06:33 -0500

    refactor: remove ListeningSection structs from repositories/listening_sections.rs

--- src/core/src/repositories/listening_sections.rs
+++ src/core/src/repositories/listening_sections.rs
@@ -1,35 +1,8 @@
-use serde::{Deserialize, Serialize};
-
 use crate::database::Db;
 use crate::error::AppError;
-
-#[derive(Debug, Serialize, Deserialize, Clone, sqlx::FromRow)]
-pub struct ListeningSection {
-    pub id: String,
-    pub test_id: String,
-    pub section_number: i64,
-    pub title: String,
-    pub transcript: Option<String>,
-    pub audio_url: Option<String>,
-    pub created_at: String,
-}
-
-#[derive(Debug, Deserialize)]
-pub struct CreateListeningSection {
-    pub test_id: String,
-    pub section_number: Option<i64>,
-    pub title: Option<String>,
-    pub transcript: Option<String>,
-    pub audio_url: Option<String>,
-}
-
-#[derive(Debug, Deserialize)]
-pub struct UpdateListeningSection {
-    pub section_number: Option<i64>,
-    pub title: Option<String>,
-    pub transcript: Option<String>,
-    pub audio_url: Option<String>,
-}
+use crate::models::listening_sections::{
+    CreateListeningSection, ListeningSection, UpdateListeningSection,
+};
 
 pub async fn find_by_id(
     pool: &Db,
